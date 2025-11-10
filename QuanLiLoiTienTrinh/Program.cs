f6d00a2ca79e7ad5aa13a93c6a53fb6612d32c24
VietHoang <viethoangcbvn0301@gmail.com>
2025-11-10 22:35:18 +0700

    chức năng login

--- QuanLiLoiTienTrinh/Program.cs
+++ QuanLiLoiTienTrinh/Program.cs
@@ -1,22 +1,37 @@
-﻿using System;
+﻿using QuanLiLoiTienTrinh;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace QuanLiLoiTienTrinh
+namespace BTL_QuanLiLoTienTrinh
 {
-    internal static class Program
+    static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmLogin());
+
+            // 1. Tạo một đối tượng của frmLogin
+            frmLogin loginForm = new frmLogin();
+
+            // 2. Hiển thị loginForm dưới dạng Dialog (hộp thoại)
+            // Code sẽ tạm dừng ở đây cho đến khi frmLogin bị đóng
+            DialogResult result = loginForm.ShowDialog();
+
+            // 3. Kiểm tra kết quả sau khi frmLogin bị đóng
+            if (result == DialogResult.OK)
+            {
+                Application.Run(new frmMain());
+            }
+
+            // 4. Nếu đăng nhập KHÔNG thành công (ví dụ: nhấn Thoát, nhấn dấu X)
+            // thì hàm Main() sẽ kết thúc và ứng dụng tự động đóng lại.
         }
     }
 }
